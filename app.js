b77b04bb1c88c7a4047d403ed884f1b43160611c
Raul Rodrigues <raulrodrigues13@gmail.com>
2025-03-04 22:50:24 -0300

    cria function adicionarAmigo() em app.js

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
+let listaDeAmigos = [];
+let  = document.querySelector('.input-name');
+let campoComListaDeAmigos = document.querySelector('.name-list');
+
+function adicionarAmigo() {
+    if (campoDeInput.value === '') {
+        //Melhorar essa mensagem.
+        alert('Por favor, insira um nome válido!');
+    } else {
+        //guarda o nome digitado na var.
+        let nomeCapturado = campoDeInput.value;
+        //seleciona a lista de amigos e sobe o nomeCapturado como parametro.
+        listaDeAmigos.push(nomeCapturado);
+        //adiciona o novo nome a lista de amigos no HTML.
+        atualizarListaDeAmigos();
+        //seleciona o campo de texto e o limpa no final da função.
+        campoDeInput.value = '';
+
+
+        //DEBUG
+        console.log(listaDeAmigos);
+    }
+
+}
 
